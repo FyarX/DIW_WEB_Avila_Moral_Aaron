46bf6e0b588352eef7999efb7d8a1d72c07e2e45
FyarX <aaronavilamoralrob@gmail.com>
2025-02-01 19:28:52 +0100

    header y footer responsive

--- fuente/js/script.js
+++ fuente/js/script.js
@@ -1,8 +1,15 @@
 function actualizarBusqueda() {
+    // Obtiene el elemento de la barra de búsqueda
     const barraBusqueda = document.getElementById('search_bar');
+
+    // Si la ventana es menor o igual a 420px, cambia el placeholder
     if (window.innerWidth <= 420) {
-      barraBusqueda.placeholder = 'Buscar...';
+      
+      // Cambia el placeholder
+      barraBusqueda.setAttribute('placeholder', 'Buscar...');
+
     } else {
+      // Si la ventana es mayor a 420px, vuelve al placeholder mas largo
       barraBusqueda.placeholder = 'Buscar libros, autores, ISBN...';
     }
   }
@@ -11,4 +18,4 @@ function actualizarBusqueda() {
   window.onload = actualizarBusqueda();
 
   // Llama a la función cuando la ventana cambia de tamaño
-  window.onresize = actualizarBusqueda();
+  window.addEventListener("resize", actualizarBusqueda);
